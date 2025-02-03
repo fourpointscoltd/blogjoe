5125b8f7fa25c412c2eb1837d0233d213a56319f
fourpointscoltd <51435476+fourpointscoltd@users.noreply.github.com>
2025-02-04 00:59:42 +0800

    11
    11

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,52 +1,13 @@
-import { Metadata } from 'next';
-import { getDetail } from '@/libs/microcms';
-import Article from '@/components/Article';
-
-type Props = {
-  params: {
-    slug: string;
-  };
-  searchParams: {
-    dk: string;
-  };
-};
-
-export const revalidate = 60;
-
-export async function generateMetadata({ params, searchParams }: Props): Promise<Metadata> {
-  const data = await getDetail(params.slug, {
-    draftKey: searchParams.dk,
-  });
-
-  return {
-    title: data.title,
-    description: data.description,
-    openGraph: {
-      title: data.title,
-      description: data.description,
-      images: [data?.thumbnail?.url || ''],
-    },
-  };
-}
-
-export default async function Page({ params, searchParams }: Props) {
-  const data = await getDetail(params.slug, {
-    draftKey: searchParams.dk,
-  });
-
-  return <Article data={data} />;
-}
-
 import { getArticleById, getAllArticles } from '@/libs/microcms'; // 先ほどの関数をインポート
 import { type Article } from '@/libs/microcms'; // Article 型をインポート
 import Article from '@/components/Article'; // Article コンポーネントをインポート
 
 export async function getStaticProps({ params }: { params: { slug: string } }) {
   // 現在の記事データを取得
-  const articleData = await getArticleById(params.slug); 
+  const articleData = await getArticleById(params.slug);
 
   // すべての記事データを取得
-  const allArticles = await getAllArticles(); 
+  const allArticles = await getAllArticles();
 
   return {
     props: {
@@ -74,4 +35,4 @@ const ArticlePage = ({ data, allArticles }: { data: Article, allArticles: Articl
   return <Article data={data} allArticles={allArticles} />;
 }
 
-export
+export default ArticlePage;
